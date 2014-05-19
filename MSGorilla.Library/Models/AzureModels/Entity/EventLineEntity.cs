fbaaf2c81ea19cf6d9d4871807c362f099e20d3f
Yi Dong Guo <guoyidong@126.com>
2014-05-19 13:02:29 +0800

    add the missed EventLineEntity

---
+++ MSGorilla.Library/Models/AzureModels/Entity/EventLineEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace MSGorilla.Library.Models.AzureModels.Entity
+{
+    public class EventLineEntity : TableEntity
+    {
+        public string Content { get; set; }
+        public EventLineEntity()
+        {
+            ;
+        }
+
+        public EventLineEntity(Message msg)
+        {
+            this.PartitionKey = string.Format("{0}_{1}", msg.EventID, 
+                Utils.ToAzureStorageDayBasedString(msg.PostTime.ToUniversalTime()));
+            this.RowKey = msg.ID;
+
+            Content = msg.ToJsonString();
+        }
+    }
+}
