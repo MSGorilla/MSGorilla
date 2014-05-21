a652057eafb4b62459ba395a42fd9839ebcf3a23
Sijia Huang <larch300@hotmail.com>
2014-05-21 13:12:28 +0800

    PlaceHolder for CFMonitor, and Gorilla webapi client

---
+++ CodeFlowMonitor/Program.cs
@@ -0,0 +1,19 @@
+﻿using MSGorilla.WebAPI.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFlowMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GorillaWebAPI webapi = new GorillaWebAPI("CFMonitor", "User@123");
+            webapi.
+            webapi.HomeLine(DateTime.UtcNow.AddDays(-1), DateTime.UtcNow);
+        }
+    }
+}
