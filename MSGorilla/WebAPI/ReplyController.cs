d2366fdea24193a1526a76c6ff09fca507620d31
fudanyi <fudanyi@gmail.com>
2014-05-21 21:34:43 +0800

    index half done

---
+++ MSGorilla/WebAPI/ReplyController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using MSGorilla.Library;
+using MSGorilla.Filters;
+using MSGorilla.Library.Models;
+using MSGorilla.Library.Exceptions;
+using MSGorilla.Library.Models.SqlModels;
+
+namespace MSGorilla.WebApi
+{
+    public class ReplyController : BaseController
+    {
+        ReplyManager _replyManager = new ReplyManager();
+
+        [HttpGet]
+        public List<Reply> Replies()
+        {
+            return _replyManager.GetAllReply(whoami());
+        }
+
+        [HttpGet]
+        public List<Reply> ReplyNotification()
+        {
+            return _replyManager.GetReplyNotif(whoami());
+        }
+
+        [HttpGet, HttpPost]
+        public ActionResult PostReply(string to, string message, string messageUser, string messageID)
+        {
+            _replyManager.PostReply(whoami(), to, message, DateTime.UtcNow, messageUser, messageID);
+            return new ActionResult();
+        }
+    }
+}
