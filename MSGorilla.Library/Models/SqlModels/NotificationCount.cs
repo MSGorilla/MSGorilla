01e0b2be1a6d96bf81a1ddebc48616d60d0cc337
ptolemy <guoyidong@126.com>
2014-05-30 17:04:30 +0800

    uncompleted notification count

---
+++ MSGorilla.Library/Models/SqlModels/NotificationCount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace MSGorilla.Library.Models.SqlModels
+{
+    [Serializable]
+    class NotificationCount
+    {
+        [Key]
+        public string Userid { get; set; }
+        public int UnreadHomelineMsgCount { get; set; }
+        public int UnreadOwnerlineMsgCount { get; set; }
+        public int UnreadUserlineMsgCount { get; set; }
+    }
+}
