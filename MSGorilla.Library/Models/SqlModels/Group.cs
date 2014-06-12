fc707af3144fa9ba6b51c4002b7e41789e09a581
Yi Dong Guo <guoyidong@126.com>
2014-06-12 11:14:55 +0800

    add group and menbership model

---
+++ MSGorilla.Library/Models/SqlModels/Group.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace MSGorilla.Library.Models.SqlModels
+{
+    [Serializable, DataContract]
+    public class Group
+    {
+        [Key, DataMember]
+        public string Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+    }
+}
---
+++ MSGorilla.Library/Models/SqlModels/Membership.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace MSGorilla.Library.Models.SqlModels
+{
+    [Serializable, DataContract]
+    public class Membership
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string GroupID { get; set; }
+        [DataMember]
+        public string EntityID { get; set; }
+        [DataMember]
+        public string Role { get; set; }
+    }
+}
