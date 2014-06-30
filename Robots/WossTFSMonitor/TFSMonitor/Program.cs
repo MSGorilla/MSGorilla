87b33be84788f91314ef13e633195000cca6a00f
coolmay <shiqiufang@hotmail.com>
2014-06-30 14:58:22 +0800

    Add Robot WossTFSMonitor

---
+++ Robots/WossTFSMonitor/TFSMonitor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MSGorilla.WebAPI.Client;
+using System.Timers;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TfsHelper.GetWOSSTableBugsLatestUpdated(DateTime.Now.AddDays(-1.0f));
+
+            System.Timers.Timer timer = new System.Timers.Timer(1000.0f * 60 * 10); // 10 minutes timer
+            // Hook up the Elapsed event for the timer. 
+            timer.Elapsed += OnTimedEvent;
+            timer.Enabled = true;
+
+            while(Console.ReadKey().KeyChar != 'q')
+            {
+                Console.WriteLine("Press q to exit");
+            }
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            TfsHelper.GetWOSSTableBugsLatestUpdated(DateTime.Now.AddMinutes(-10.0f));
+        }
+    }
+}
---
+++ Robots/WossTFSMonitor/TFSMonitor/tfsHelper.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Microsoft.EngineeringServices.DataContracts.Model;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System.Net;
+using System.Windows.Forms;
+using MSGorilla.WebAPI.Client;
+
+namespace Test
+{
+    public class TfsHelper
+    {
+        static TfsTeamProjectCollection tpc = null;
+
+        private static string DisplayName2Alias(string dispName)
+        {
+            if (string.IsNullOrEmpty(dispName))
+            {
+                return "";
+            }
+            if (Constants.disp2aliasMap.ContainsKey(dispName.ToLower()))
+            {
+                return Constants.disp2aliasMap[dispName.ToLower()];
+            }
+            return dispName;
+        }
+
+        public static void GetWOSSTableBugsLatestUpdated(DateTime lastCheckTime)
+        {
+            
+            string bugLinkTemplate = "https://microsoft.visualstudio.com/DefaultCollection/WSSC/_workitems";
+            string bugLinkDataTemplate = "#_a=edit&id={0}&triage=true";
+            string[] monitoredFields = new string[] {
+                        "Title", 
+                        "Work Item Type",
+                        "Issue Type",
+                        "Severity",
+                        "State",
+                        "Reason",
+                        "Resolved By",
+                        "Iteration Path",
+                        "Assigned To",
+                        "Activated By",
+                        "Priority",
+                        "Found In",
+                        "Task Type",
+                        "Triage",
+                        "ID",
+                        "SD Changelist"
+                        };
+
+            string fieldChangeTemplate =
+                "<div style=\"display: block;\">" +
+                    "<div class=\"container\">" +
+                        "<table class=\"detail-list\">" +
+                            "<tbody>" +
+                                "<tr><td>Field</td><td>New value</td><td>Old Value</td></tr>" +
+                                "fieldChangeRows" +
+                            "</tbody>" +
+                        "</table>" +
+                    "</div>" +
+                "</div>";
+            if (tpc == null)
+            {
+                TeamProjectPicker tpp = new TeamProjectPicker(TeamProjectPickerMode.SingleProject, false);
+                if (tpp.ShowDialog() == DialogResult.OK)
+                {
+                    tpc = tpp.SelectedTeamProjectCollection;
+                }
+            }
+            if (tpc != null)
+            {
+                try
+                {
+                    WorkItemStore workItemStore = tpc.GetService<WorkItemStore>(); var workItems = workItemStore.Query(String.Format(@"
+                   Select [Title]
+                   From WorkItems
+                   Where [Work Item Type] = 'Bug'
+                   AND [Area Path] Under 'WSSC\WSSC-Windows Server and System Center\FSC-File Server and Clustering\WOSS'
+                   AND [Issue Type] = 'Code Defect'
+                   AND [Changed Date] > '{0}'", lastCheckTime.Date.AddDays(-1.0f)));
+
+                    // Update existing work item
+                    if (workItems.Count > 0)
+                    {
+                        foreach (WorkItem wi in workItems)
+                        {
+                            if (wi.ChangedDate.CompareTo(lastCheckTime) < 0)
+                            {
+                                continue;
+                            }
+
+                            string message = "";
+
+                            int id = wi.Id;
+                            string owner = DisplayName2Alias(wi.CreatedBy);
+                            string title = wi.Title;
+                            string issueType = (string)wi.Fields["Issue Type"].Value;
+                            string assignTo = DisplayName2Alias((string)wi.Fields["Assigned To"].Value);
+                            string build = (string)wi.Fields["Found In"].Value;
+                            string changeBy = DisplayName2Alias(wi.ChangedBy);
+                            string closeBy = DisplayName2Alias((string)wi.Fields["Closed By"].Value);
+                            string resolveBy = DisplayName2Alias((string)wi.Fields["Resolved By"].Value);
+                                                                                    
+                            if (wi.Revision == 1)// new created
+                            {
+                                message = string.Format("#WOSS TFS Created# #WOSS TFS {0}# Created by @{1}\n", id, owner);
+                                message += "Bug Title: " + title + "\n";
+                                message += "Bug Assigned to @" + assignTo + "\n";
+                                if (!string.IsNullOrEmpty(build)) message += "Bug found in build #WOSS Build " + build + "#\n";
+                            }
+                            else if (wi.ChangedBy == "Payload Tracking (microsoft)") // resolved by SD check-in
+                            {
+                                message = string.Format("#WOSS TFS Checkin# #WOSS TFS {0}# has check-in #WOSS Change {1}#  by @{2}\n", id, wi.Fields["SD Changelist"].Value, wi.Fields["SD User"].Value);
+                            }
+                            else if (wi.State != (string)(wi.Fields["State"].OriginalValue)) // State Changes
+                            {
+                                if (wi.State == "Active")
+                                {
+                                    message = string.Format("#WOSS TFS Activated# #WOSS TFS {0}# Activated by @{1}\n", id, wi.Fields["Activated By"].Value);
+                                }
+                                else if (wi.State == "Closed")
+                                {
+                                    message = string.Format("#WOSS TFS Closed# #WOSS TFS {0}# Closed by @{1}\n", id, closeBy);
+                                }
+                                else
+                                {
+                                    message = string.Format("#WOSS TFS Resolved# #WOSS TFS {0}# Resolved by @{1}\n", id, resolveBy);
+                                }
+                            }
+                            else // Other changes
+                            {
+                                message = string.Format("#WOSS TFS Changed# #WOSS TFS {0}# changed by @{1} ({2})\n", wi.Id, changeBy, wi.ChangedBy);
+                            }
+
+                            message += "\nField Changes:\n";
+                            StringBuilder richMessage = new StringBuilder();
+                            var revLatest = wi.Revisions[wi.Revision - 1];
+                            foreach (var fieldName in monitoredFields)
+                            {
+                                if (revLatest.Fields[fieldName].Value != null && revLatest.Fields[fieldName].Value != revLatest.Fields[fieldName].OriginalValue)
+                                {
+                                    if (revLatest.Fields[fieldName].Value.GetType() == typeof(System.Int32))
+                                    {
+                                        var cV = (int)revLatest.Fields[fieldName].Value;
+                                        var oV = (int)(revLatest.Fields[fieldName].OriginalValue == null ? 0 : revLatest.Fields[fieldName].OriginalValue);
+                                        if (cV == oV)
+                                        {
+                                            continue;
+                                        }
+                                    }
+
+                                    string oriValue = (string)revLatest.Fields[fieldName].OriginalValue;
+                                    if (string.IsNullOrEmpty(oriValue)) oriValue = "(null)";
+                                    message += string.Format("  Field {0}: {1} --> {2} \n", revLatest.Fields[fieldName].Name, oriValue, revLatest.Fields[fieldName].Value);
+                                    richMessage.Append(string.Format("<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>", revLatest.Fields[fieldName].Name, revLatest.Fields[fieldName].Value, oriValue));
+                                }
+                            }
+
+                            //message += "Bug link: " + bugLinkTemplate + Uri.EscapeDataString(string.Format(bugLinkDataTemplate, id));
+                            message += "Bug link: " + bugLinkTemplate + string.Format(bugLinkDataTemplate, id);
+
+                            Console.WriteLine(message);
+                            // twitter message
+                            try
+                            {
+                                GorillaWebAPI webAPI = new GorillaWebAPI("User1", "111111");
+                                webAPI.PostMessage(message, "none", id.ToString(), new string[] { "WOSS TFS" }, new string[] { owner }, new string[] { assignTo, resolveBy, closeBy, changeBy });
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Exception happen when twittering: " + e);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception throw: " + e);
+                    return;
+                }
+
+            }
+        }
+
+    }
+}
