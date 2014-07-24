456e5c12ecb5bea5c674489145a835158653dbb4
fudanyi <fudanyi@gmail.com>
2014-07-24 15:22:31 +0800

    buf fix: gettopicname

---
+++ MSGorilla.Library/SearchManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MSGorilla.Library.Azure;
+using MSGorilla.Library.Models;
+using MSGorilla.Library.Models.SqlModels;
+using MSGorilla.Library.Models.AzureModels;
+using MSGorilla.Library.Models.ViewModels;
+using MSGorilla.Library.Exceptions;
+using MSGorilla.Library.Models.AzureModels.Entity;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace MSGorilla.Library
+{
+    public class SearchManager
+    {
+        public void SpideMessage(Message message)
+        {
+
+        }
+
+        public void Search(string keywords)
+        {
+
+        }
+    }
+}
