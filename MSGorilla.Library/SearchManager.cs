23e2c12afa5edfa187ad9765e1123b3299ab5269
fudanyi <fudanyi@gmail.com>
2014-07-24 16:54:36 +0800

    backup

--- MSGorilla.Library/SearchManager.cs
+++ MSGorilla.Library/SearchManager.cs
@@ -15,18 +15,40 @@ using MSGorilla.Library.Models.AzureModels.Entity;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
+using mshtml;
+
+
 namespace MSGorilla.Library
 {
     public class SearchManager
     {
         public void SpideMessage(Message message)
         {
+            // convert message to text string
+            string msgText = GenarateMessageString(message);
 
+            // segment msg text string to word list
+
+            // count word list 
+
+            // add count result into table
+
+            HTMLDocumentClass doc = new HTMLDocumentClass();
+            doc.designMode = "on";
+            doc.IHTMLDocument2_write("some html string");
+            string richmsg = doc.body.innerText;
         }
 
         public void Search(string keywords)
         {
 
+        }
+
+        private string GenarateMessageString(Message message)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            return sb.ToString();
         }
     }
 }
